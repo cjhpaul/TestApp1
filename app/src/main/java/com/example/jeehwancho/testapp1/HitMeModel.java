4b822272fbd2cd1a5709beee8ca544238b590668
jeehwancho <jeehwan.cho@gmail.com>
2015-06-26 00:40:11 -0400

    added 16 pikachus in 4 by 4 grid

--- app/src/main/java/com/example/jeehwancho/testapp1/HitMeModel.java
+++ app/src/main/java/com/example/jeehwancho/testapp1/HitMeModel.java
@@ -8,27 +8,28 @@ import android.graphics.Canvas;
  */
 public class HitMeModel {
     private Bitmap m_bitmap;
-    private int m_X, m_Y;
+    private float m_X, m_Y;
+    private boolean m_isVisible;
 
-    public HitMeModel(Bitmap bitmap) {
+    public HitMeModel(Bitmap bitmap, float x, float y) {
         m_bitmap = bitmap;
-        m_X = 0;
-        m_Y = 0;
-    }
-
-    public void setX(int x) {
         m_X = x;
+        m_Y = y;
+        m_isVisible = false; //not visible by default
     }
 
-    public void setY(int y) {
+    public void setXY(float x, float y) {
+        m_X = x;
         m_Y = y;
     }
 
     public void draw(Canvas canvas) {
-        canvas.drawBitmap(m_bitmap, m_X, m_Y, null);
+        if (m_isVisible) {
+            canvas.drawBitmap(m_bitmap, m_X, m_Y, null);
+        }
     }
 
-    public void update() {
-        //do update
+    public void update(boolean isVisible) {
+        m_isVisible = isVisible;
     }
 }
--- app/src/main/java/com/example/jeehwancho/testapp1/SingleGamePanel.java
+++ app/src/main/java/com/example/jeehwancho/testapp1/SingleGamePanel.java
@@ -2,13 +2,20 @@ package com.example.jeehwancho.testapp1;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import junit.framework.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jeehwancho on 6/22/15.
@@ -17,13 +24,30 @@ public class SingleGamePanel extends SurfaceView implements SurfaceHolder.Callba
     private static final String TAG = SingleGamePanel.class.getSimpleName();
     private SingleGameThread m_singleGameThread;
     private HitMeModel m_hitMeModel;
+    private float m_h, m_w;
+    private List<HitMeModel> m_models;
+    private Bitmap m_bitmapPikachu;
 
     public SingleGamePanel(Context context) {
         super(context);
         getHolder().addCallback(this);
         m_singleGameThread = new SingleGameThread(getHolder(), this);
-        m_hitMeModel = new HitMeModel(BitmapFactory.decodeResource(getResources(), R.drawable.pikachu));
         setFocusable(true);
+        m_models = new ArrayList();
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        m_h = metrics.heightPixels / 10;
+        m_w = metrics.widthPixels / 4;
+        m_bitmapPikachu = BitmapFactory.decodeResource(getResources(), R.drawable.pikachu);
+        m_bitmapPikachu = Bitmap.createScaledBitmap(m_bitmapPikachu, (int) m_w, (int) m_h * 2, true);
+        m_hitMeModel = new HitMeModel(m_bitmapPikachu, 0, 0);
+
+        //generate 4by4 models
+        for (int i = 1; i < 9; i += 2)
+            for (int j = 0; j < 4; j++)
+                m_models.add(new HitMeModel(m_bitmapPikachu, m_w * j, m_h * i));
+        Assert.assertEquals(16, m_models.size());
     }
 
     @Override
@@ -58,8 +82,7 @@ public class SingleGamePanel extends SurfaceView implements SurfaceHolder.Callba
                 m_singleGameThread.setRunning(false);
                 ((Activity) getContext()).finish();
             } else {
-                m_hitMeModel.setX((int) event.getX());
-                m_hitMeModel.setY((int) event.getY());
+                m_hitMeModel.setXY(event.getX(), event.getY());
                 Log.d(TAG, "x=" + event.getX() + ",y=" + event.getY());
             }
         }
@@ -67,12 +90,15 @@ public class SingleGamePanel extends SurfaceView implements SurfaceHolder.Callba
     }
 
     public void update() {
-        //update status
-        m_hitMeModel.update();
+        m_hitMeModel.update(true);
+        for (HitMeModel model : m_models)
+            model.update(true);
     }
 
     public void render(Canvas canvas) {
         canvas.drawColor(Color.BLACK);
         m_hitMeModel.draw(canvas);
+        for (HitMeModel model : m_models)
+            model.draw(canvas);
     }
 }
