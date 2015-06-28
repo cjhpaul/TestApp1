7be70aa1031a77134b32a78474278ed4573ec9f6
jeehwancho <jeehwan.cho@gmail.com>
2015-06-27 22:19:16 -0400

    FPS set to 40. CPU max spike around 30%, which happens infrequently. It was steady 30% before.

--- app/src/main/java/com/example/jeehwancho/testapp1/SingleGameThread.java
+++ app/src/main/java/com/example/jeehwancho/testapp1/SingleGameThread.java
@@ -6,9 +6,13 @@ import android.view.SurfaceHolder;
 
 /**
  * Created by jeehwancho on 6/22/15.
+ * Thread class that keeps max FPS
  */
 public class SingleGameThread extends Thread {
     private static final String TAG = SingleGameThread.class.getSimpleName();
+    private final static int MAX_FPS = 40; // desired fps
+    private final static int MAX_FRAME_SKIPS = 5;
+    private final static int FRAME_PERIOD = 1000 / MAX_FPS;
     private SurfaceHolder m_surfaceHolder;
     private SingleGamePanel m_singleGamePanel;
     private boolean m_isRunning;
@@ -27,13 +31,35 @@ public class SingleGameThread extends Thread {
     public void run() {
         Canvas canvas;
         Log.d(TAG, "Run has begun.");
+        long beginTime;
+        long timeDiff;
+        int sleepTime;
+        int framesSkipped;
         while (m_isRunning) {
             canvas = null;
             try {
                 canvas = m_surfaceHolder.lockCanvas();
                 synchronized (m_surfaceHolder) {
+                    beginTime = System.currentTimeMillis();
+                    framesSkipped = 0;
                     m_singleGamePanel.update();
                     m_singleGamePanel.render(canvas);
+                    timeDiff = System.currentTimeMillis() - beginTime;
+
+                    sleepTime = (int) (FRAME_PERIOD - timeDiff);
+                    if (sleepTime > 0) {
+                        try {
+                            Thread.sleep(sleepTime);
+                        } catch (InterruptedException e) {
+                            Log.d(TAG, e.getMessage());
+                        }
+                    }
+
+                    while (sleepTime < 0 && framesSkipped < MAX_FRAME_SKIPS) {
+                        this.m_singleGamePanel.update(); // update without rendering
+                        sleepTime += FRAME_PERIOD;
+                        framesSkipped++;
+                    }
                 }
             } finally {
                 if (canvas != null)
